c3881579297ae6aa5df083170f7a0752ecbb4955
Arunsk00 <arunsk199514@gmail.com>
2024-05-23 14:26:06 +0530

    conflict

--- src/org/college/College.java
+++ src/org/college/College.java
@@ -6,15 +6,7 @@ import java.util.List;
 public class College {
 
 	public static void main(String[] args) {
-//		for(int i =1;i<=5;i++) {
-//			for(int j=1;j<=i;j++) {
-//				
-//				if(j%2!=0) {
-//				System.out.print("*");
-//				}
-//				else {
-//					System.out.print("_");
-//				}
+
 			
 		List<String> a=new ArrayList<>();
 		a.add("fa");
