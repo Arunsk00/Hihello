a85975ba4f25cf2d4500aa3e6d4b1dbea86ad468
Arunsk00 <arunsk199514@gmail.com>
2024-05-23 14:07:12 +0530

    conf

--- src/org/college/College.java
+++ src/org/college/College.java
@@ -1,25 +1,35 @@
 package org.college;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class College {
 
 	public static void main(String[] args) {
-		for(int i =1;i<=5;i++) {
-			for(int j=1;j<=i;j++) {
-				
-				if(j%2!=0) {
-				System.out.print("*");
-				}
-				else {
-					System.out.print("_");
-				}
-			}
+//		for(int i =1;i<=5;i++) {
+//			for(int j=1;j<=i;j++) {
+//				
+//				if(j%2!=0) {
+//				System.out.print("*");
+//				}
+//				else {
+//					System.out.print("_");
+//				}
+//			}
+		List<String>a=new ArrayList<>();
+		a.add("fa");
+		a.add("moh");
+		for (String string : a) {
+			System.out.println(string);
+		}
+		
 			System.out.println("Changes made here is in same branch");
 		}
-	}
-
-
-
-
-
+	
 
 }
+
+
+
+
+
