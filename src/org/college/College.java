b9255552921a97907005f38423c20f73393abc9e
Arunsk00 <arunsk199514@gmail.com>
2024-05-23 14:40:40 +0530

    message object creation done

--- src/org/college/College.java
+++ src/org/college/College.java
@@ -4,21 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class College {
+	
+	public void first() {
+System.out.println("first");
+	}
+	public void second() {
+		System.out.println("second");
+
+	}
 
 	public static void main(String[] args) {
-//		for(int i =1;i<=5;i++) {
-//			for(int j=1;j<=i;j++) {
-//				
-//				if(j%2!=0) {
-//				System.out.print("*");
-//				}
-//				else {
-//					System.out.print("_");
-//				}
-//			}
+		College c = new College();
+		c.first();
+		c.second();
+
 		List<String>a=new ArrayList<>();
 		a.add("fa");
 		a.add("moh");
+	
 		for (String string : a) {
 			System.out.println(string);
 		}
